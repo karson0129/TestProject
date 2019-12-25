67803018097e5203543ee17c5f31cd37bc749fd5
karson <k81904791@163.com>
2019-12-25 22:53:01 +0800

    修改了kotlin代理模式的bug

--- app/src/main/java/com/github/myapplication/ProxyMode/MovingProxy/Mall.kt
+++ app/src/main/java/com/github/myapplication/ProxyMode/MovingProxy/Mall.kt
@@ -7,19 +7,23 @@ import java.util.*
 
 class Mall :InvocationHandler{
 
-    var obj:Object? = null
+    var obj: Object? = null
 
     fun getNewIntent(): Any {
         return Proxy.newProxyInstance(obj?.`class`?.classLoader,
              obj?.`class`?.interfaces,this@Mall)
     }
 
-    override fun invoke(any: Any?, method: Method?, p2: Array<out Any>?): Any {
+    override fun invoke(any: Any?, method: Method?, args: Array<out Any>?): Any {
+        println("before http request--->$args")
         yitialong()
-        val invoke = method?.invoke(obj, p2)
+//        val invoke = method?.invoke(obj, *(p2))
+        //这里是kotlin坑 args参数如果直接放进去会报错 必须写成 *(args ?: arrayOfNulls<Any>(0))
+        val ret = method!!.invoke(obj, *(args ?: arrayOfNulls<Any>(0)) )
         wanyi()
-        return invoke!!
+        return ret
     }
+
 
     fun yitialong(){
         println("一条龙服务")
